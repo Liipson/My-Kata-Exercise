8669d8ada4c51e9d9d3147d7d5bde93e39503ce2
Lipson <liipson@wp.pl>
2022-08-09 00:52:41 +0200

    .

--- Kata Exercise/Program.cs
+++ Kata Exercise/Program.cs
@@ -6,8 +6,11 @@ namespace Kata_Exercise
     {
         static void Main(string[] args)
         {
-           //Console.WriteLine(Kyu8Kata.DrawStairs(6));
-           // Console.WriteLine(Kyu8Kata.SpeakEnglish("dfsbgjdfgjnjkfnEnglishkjndfgssdfkjn"));
+            /*
+           Console.WriteLine(Kyu8Kata.DrawStairs(6));
+            Console.WriteLine(Kyu8Kata.SpeakEnglish("dfsbgjdfgjnjkfnEnglishkjndfgssdfkjn"));
+            */
+
 
         }
     }
