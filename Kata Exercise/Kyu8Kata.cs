04c7351ec3f9792f0908e5cdbd0c0512882d6a49
Lipson <liipson@wp.pl>
2022-08-09 02:21:11 +0200

    :)

--- Kata Exercise/Kyu8Kata.cs
+++ Kata Exercise/Kyu8Kata.cs
@@ -99,12 +99,14 @@ namespace Kata_Exercise
 
 
 
+
+
+        ------FIND THE SMALLEST INTEGER IN THE ARRAY----
         Given an array of integers your solution should find the smallest integer.
         For example:
         Given [34, 15, 88, 2] your solution will return 2
         Given [34, -345, -1, 100] your solution will return -345
         */
-
         public static int FindSmallestInt(int[] args)
         {
             int smallestValue = args[0];
