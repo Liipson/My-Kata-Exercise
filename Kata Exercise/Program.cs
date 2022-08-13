8d902c66b8ab39ba947423719a6c666bd5868558
Lipson <liipson@wp.pl>
2022-08-13 21:14:05 +0200

    .

--- Kata Exercise/Program.cs
+++ Kata Exercise/Program.cs
@@ -12,10 +12,11 @@ namespace Kata_Exercise
             Console.WriteLine(Kyu8Kata.SameCase('a', '*'));
             Console.WriteLine(Kyu8Kata.FindSmallestInt(new int[] { 34, 15, 88, 2 }));
             Console.WriteLine(Kyu8Kata.Hero(10, 5));
+            Console.WriteLine(Kyu8Kata.Litres(5));
+            Console.WriteLine(Kyu8Kata.Maps(new int[] { 2,6,3,4,56})); 
             */
 
-            Console.WriteLine(Kyu8Kata.Litres(5));
-
+            Console.WriteLine(Kyu8Kata.SwitchItUp(5));
         }
     }
 }
